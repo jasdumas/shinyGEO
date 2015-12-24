4381a7680eaabc5dca01fd7a2dfbcb12ac06f073
Mike G <mgdlxd@gmail.com>
2015-12-23 23:22:25 -0500

    Update this resizes data table modal and resets the progress bar when other tabs are clicked.

--- www/ecsu.js
+++ www/ecsu.js
@@ -26,5 +26,19 @@ $('#downloadSet').click(function(){
    $('#ioAlert').children('div').children('p').html("<p><strong>Your dataset  has been downloaded!</strong> Edit your data then save your work and upload the dataset.</p>")
 
 });
+
+$('.nav-tabs > li:nth-child(2)').click(function(event){
+	$('.progress-bar').css("width",0);
+
+});
+$('.nav-tabs > li:nth-child(1)').click(function(event){
+	$('.progress-bar').css("width",0);
+
+
+});
+
+
 }); 
+
+
 
