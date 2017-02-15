0691e3d9ff6c6e28490fabbe7878c2f8404e48a7
jasdumas <jasmine.dumas@gmail.com>
2017-02-15 09:48:45 -0500

    add package level documentation

---
+++ R/shinyGEO.R
@@ -0,0 +1,14 @@
+#' shinyGEO: A package for analyzing gene expression expression data with shiny.
+#'
+#' The shinyGEO package provides two important functions:
+#' updateGEOdata, runshinyGEO.
+#'
+#' @section shinyGEO functions:
+#' updateGEOdata() updates the meta-data for populating the search
+#' box for series and platform data from the Gene Expression Omnibus.
+#' runshinyGEO() launches the shiny application locally.
+#'
+#' @docType package
+#' @name shinyGEO
+#' @aliases shinyGEO-package
+NULL
