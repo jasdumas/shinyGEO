8b3f523f52f85f11df49687a16b37e411bed353d
jasdumas <jasmine.dumas@gmail.com>
2017-02-19 12:29:54 -0500

    set-up defacto testthat suite - non functioning test

--- tests/testthat/testupdateGEOdata.R
+++ tests/testthat/testupdateGEOdata.R
@@ -1,13 +1,15 @@
 library(shinyGEO)
 context("Update meta data")
 
-test_that("updateGEOdata generates csv files", {
-  skip_on_cran()
-  skip_if_not_installed()
-  expect_output_file(updateGEOdata(), file = "inst/extdata/series.csv", update=NA)
-  #expect_output_file(updateGEOdata(), file = "inst/extdata/platform.csv", update=T)
+# test_that("updateGEOdata starts", {
+#   expect_null(updateGEOdata())
+# })
 
-})
+# test_that("updateGEOdata generates csv files", {
+#   expect_output_file(updateGEOdata(), file = "inst/extdata/series.csv", update=F)
+#   expect_output_file(updateGEOdata(), file = "inst/extdata/platform.csv", update=T)
+#
+# })
 
 # test_that("updateGEOdata generates rda files", {
 #   skip_on_cran()
